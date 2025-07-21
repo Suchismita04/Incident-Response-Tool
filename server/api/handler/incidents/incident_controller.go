bd77f77a3534c8abe779712c438ee1c51659f1a2
Suchismita04 <suchismitachatterjee13@gmail.com>
2025-07-21 22:13:24 +0530

    send data

--- server/api/handler/incidents/incident_controller.go
+++ server/api/handler/incidents/incident_controller.go
@@ -21,14 +21,15 @@ func GetIncidents(w http.ResponseWriter, r *http.Request) {
 	user := os.Getenv("WAZUH_USER")
 	pass := os.Getenv("WAZUH_PASS")
 	api := os.Getenv("WAZUH_API")
+	baseURL := fmt.Sprintf("https://%s", api)
 
-	url := fmt.Sprintf("https://%s/security/user/authenticate", api)
+	url := baseURL + "/security/user/authenticate"
 
 	// make a http request to the wazuh server
 	req, _ := http.NewRequest("POST", url, nil)
 	req.SetBasicAuth(user, pass)
 	req.Header.Set("Content-Type", "application/json")
-	fmt.Print("token from server\n", req)
+	// fmt.Print("token from server\n", req)
 
 	// this will skip the browsers specific ssl error
 	tr := &http.Transport{
@@ -47,10 +48,56 @@ func GetIncidents(w http.ResponseWriter, r *http.Request) {
 
 	defer res.Body.Close()
 
-	body, _ := io.ReadAll(res.Body)
+	authbody, _ := io.ReadAll(res.Body)
 
-	fmt.Print("body from get incident controller :\n", body)
+	/**
+	@ extract the token from res
+	*/
 
+	var authResp struct {
+		Data struct {
+			Token string `json:"token"`
+		} `json:"data"`
+	}
+	if err := json.Unmarshal(authbody, &authResp); err != nil {
+		util.ThrowApiError("Failed to parse auth token", 500)
+
+	}
+
+	token := authResp.Data.Token
+	if token == "" {
+		util.ThrowApiError("Token not found in response", 500)
+	}
+
+	agentUrl := baseURL + "/alerts"
+
+	agentReq, err := http.NewRequest("GET", agentUrl, nil)
+	if err != nil {
+		util.ThrowApiError("Failed to create agents request", 500)
+
+	}
+
+	agentReq.Header.Set("Authorization", "Bearer "+token)
+	agentReq.Header.Set("Content-Type", "application/json")
+
+	agentRes, err := client.Do(agentReq)
+	if err != nil {
+		util.ThrowApiError("Failed to get agents from Wazuh", 500)
+
+	}
+
+	agentData, err := io.ReadAll(agentRes.Body)
+
+	defer agentRes.Body.Close()
+	if err != nil {
+		util.ThrowApiError("Failed to read agents response", 500)
+
+	}
+	// fmt.Println("agentsRes is nil?", agentRes == nil)
+	// fmt.Println("agentsRes.Body is nil?", agentRes != nil && agentRes.Body == nil)
+
+	fmt.Print("data from agent:\n", string(agentData))
 	util.SetHeaders(w)
-	json.NewEncoder(w).Encode(string(body))
+	w.WriteHeader(agentRes.StatusCode)
+	w.Write(agentData)
 }
